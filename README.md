82e4b82e9a908e08031932c4b956e94159a3047f
simunda <sim@unda.ac.id>
2023-01-04 17:49:38 +0000

    tes

--- README.md
+++
@@ -1,2 +0,0 @@
-# SIM-LPPM
-Sistem Informasi Manajemen Lembaga Penelitian &amp; Pengabdian Kepada Masyarakat Perguruan Tinggi
--- simlppm/lppm/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LppmConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lppm'
